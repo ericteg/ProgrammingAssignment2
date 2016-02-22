10ba6f6395c12139c6193249f9fcaa0e116b8d25
Eric Tegenfeldt <ericteg80@gmail.com>
2016-02-21 18:11:24 -0800

    Added function code to RPeng's
    Added function code for MakeCacheMatrix and CacheSolve functions to the
    branched copy of the skeleton file provided in Roger Peng’s master copy

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
+        ## x is a maxtrix that is invertible
+        
+        ## Function will set the value of the matrix,    
+        ## determine its inverse, and cache the result.
 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <-function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## x is an invertible matrix
+
+        ## This function first checks to see if the inverse of x   
+        ## has been cached, and if so, returns it.  Otherwise, it
+        ## determines the inverse, stores it to cache, and then
+        ## returns it.
+        
+        m <-x$getsolve()
+        if (!is.null(m)) {
+                message("Returned cached inverse of x")
+                return(m)
+        }
+        
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
